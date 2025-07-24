2bf3cfbcbd54fff6c49284a2cda70212eb2893c2
Alexander Droste <alexander.droste@protonmail.com>
2025-07-24 19:07:34 +0100

    chore: build extension with rust v2 rewrite (#14)
    * chore: build extension with rust v2 rewrite
    
    * install llvm
    
    * vortex latest
    
    * libclang path
    
    * try libclang via apt get
    
    * libclang via apt-get
    
    * drop log lines
    
    * wip
    
    Signed-off-by: Alexander Droste <alexander.droste@protonmail.com>
    
    * bump vortex
    
    Signed-off-by: Alexander Droste <alexander.droste@protonmail.com>
    
    * wip
    
    Signed-off-by: Alexander Droste <alexander.droste@protonmail.com>
    
    * update
    
    ---------
    
    Signed-off-by: Alexander Droste <alexander.droste@protonmail.com>
    Co-authored-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- src/vortex_extension.cpp
+++ src/vortex_extension.cpp
@@ -1,25 +1,21 @@
 #define DUCKDB_EXTENSION_MAIN
 
-#include "duckdb/main/extension_util.hpp"
-
 #include "vortex_extension.hpp"
-#include "vortex_write.hpp"
-#include "vortex_scan.hpp"
+#include "vortex.h"
 
 using namespace duckdb;
 
-// The entry point class API can't be scoped to the vortex namespace.
+static void LoadInternal(DatabaseInstance &db_instance) {
+	vortex_init(reinterpret_cast<duckdb_database>(&db_instance));
+}
 
 /// Called when the extension is loaded by DuckDB.
 /// It is responsible for registering functions and initializing state.
 ///
 /// Specifically, the `read_vortex` table function enables reading data from
 /// Vortex files in SQL queries.
-void VortexExtension::Load(DuckDB &db) {
-	DatabaseInstance &instance = *db.instance;
-
-	vortex::RegisterWriteFunction(instance);
-	vortex::RegisterScanFunction(instance);
+void VortexExtension::Load(duckdb::DuckDB &db) {
+	LoadInternal(*db.instance);
 }
 
 /// Returns the name of the Vortex extension.
@@ -36,18 +32,9 @@ std::string VortexExtension::Name() {
 
 //! Returns the version of the Vortex extension.
 std::string VortexExtension::Version() const {
-	return "0.1.0";
+	return "0.41.2";
 }
 
-extern "C" {
-__attribute__((__visibility__("default")))
-DUCKDB_EXTENSION_API void vortex_init(duckdb::DatabaseInstance &db) {
-	duckdb::DuckDB db_wrapper(db);
-	db_wrapper.LoadExtension<VortexExtension>();
-}
-
-__attribute__((__visibility__("default")))
-DUCKDB_EXTENSION_API const char *vortex_version() {
-	return duckdb::DuckDB::LibraryVersion();
-}
-}
+#ifndef DUCKDB_EXTENSION_MAIN
+#error DUCKDB_EXTENSION_MAIN not defined
+#endif
